a8adb8c7dc4a2baff75291e4902e4d4eeac40775
LaersonJunior <laersontamorim@gmail.com>
2025-01-29 21:22:50 -0300

    Dataframe criado

--- dataframe_api.py
+++ dataframe_api.py
@@ -1,13 +1,27 @@
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as F
+from pyspark.sql import functions as Fu
 
 
 # Inicializando a sessão do Spark
 spark = (
     SparkSession.builder
-    .appName("Exemplo de DataFrame")
+    .master("loca[*]")
+    .appName("spark_dataframe_api")
     .master("local[*]")  # Define o modo de execução local com todos os núcleos disponíveis
     .getOrCreate()
 )
 
+df=(
+    spark
+    .read
+    .option('delimiter',';')
+    .option('header','true')
+    .option('inferSchama','true')
+    .option('econding','ISO-8859-1')
+    .csv('./Dados/precos-gasolina-etanol-12.csv')
+)
+
+df_precos = (
+    df.select('Estado - Sigla','Produto','Valor de Compra','Valor de Venda','Unidade de Medida')
+)
 
