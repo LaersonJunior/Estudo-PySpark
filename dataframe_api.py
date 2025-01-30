d5049b71eb1e13097f5e1957382effd382b8df74
LaersonJunior <laersontamorim@gmail.com>
2025-01-29 21:53:36 -0300

    Done

--- dataframe_api.py
+++ dataframe_api.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as Fu
+import pandas as pd
 
 
 # Inicializando a sessão do Spark
@@ -22,6 +23,60 @@ df=(
 )
 
 df_precos = (
-    df.select('Estado - Sigla','Produto','Valor de Compra','Valor de Venda','Unidade de Medida')
+    df
+    .select('Estado - Sigla','Produto','Valor de Venda','Unidade de Medida')
+    .withColumn(
+        'Valor de Venda',
+        Fu.regexp_replace(Fu.col('Valor de Venda'),",",".")
+        .cast("float")
+    )
+    
 )
 
+(
+    df_precos
+    .groupBy(
+        Fu.col('Estado - Sigla'),
+        Fu.col('Produto'),
+        Fu.col('Unidade de Medida')
+        
+    )
+    .agg(
+        Fu.min(Fu.col('Valor de Venda')).alias('Menor_valor'),
+        Fu.max(Fu.col('Valor de Venda')).alias('Maior_valor')
+    )
+    .withColumn(
+        'diferenca',
+        Fu.col('Maior_valor') - Fu.col('Menor_valor')
+    )
+    
+    .orderBy('diferenca',ascending=False)
+    
+)
+
+df_precos_analises = (
+    df_precos
+    .groupBy(
+        Fu.col('Estado - Sigla'),
+        Fu.col('Produto'),
+        Fu.col('Unidade de Medida')
+        
+    )
+    .agg(
+        Fu.min(Fu.col('Valor de Venda')).alias('Menor_valor'),
+        Fu.max(Fu.col('Valor de Venda')).alias('Maior_valor')
+    )
+    .withColumn(
+        'diferenca',
+        Fu.col('Maior_valor') - Fu.col('Menor_valor')
+    )
+    
+    .orderBy('diferenca',ascending=False)
+    
+)
+
+df_pandas = df_precos_analises.toPandas()
+
+df_pandas.to_excel('teste.xlsx',index=False)
+
+print("Finalizado")
